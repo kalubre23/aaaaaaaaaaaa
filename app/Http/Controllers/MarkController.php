57bc06a07cf502cc0c97fae990fb21dd9f0bdfbb
David Dukic <david.dukic55@gmail.com>
2024-07-13 02:14:08 +0200

    pregled ocjena

--- app/Http/Controllers/MarkController.php
+++ app/Http/Controllers/MarkController.php
@@ -12,21 +12,48 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Log;
+use InvalidArgumentException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class MarkController extends Controller
 {
 
-    public function marks(Request $request, Subject $subject, User $student)
+    public function marks(Request $request, Subject $subject, User $student = null)
     {
         Gate::authorize('viewAny', Mark::class);
 
-        try {
-            $marks = Mark::where('subject_id', $subject->id);
-            dd($marks);
+        $perPage = empty($request->perPage) ? 10 : $request->perPage;
 
+        try {
+            
+            if (auth()->user()->role->name === 'Student') {
+                $student = auth()->user();
+            }
+            if (empty($student) && in_array(auth()->user()->role->name, ['Parent'])) {
+                throw new HttpException(400, 'Student parameter is required.');
+            }
+            if (auth()->user()->role->name === 'Parent' && auth()->user()->id != $student->parent_id) {
+                return response()->json([
+                    'message' => 'You do not have access to this resource.'
+                ]);
+            }
+
+            $marks = Mark::with(['student', 'subject']);
+            if (auth()->user()->role->name === 'Admin') {
+                $marks->with('insertedBy');
+            }
+
+            $marks = $marks->where('subject_id', $subject->id);
+            if (!empty($student)) {
+                $marks = $marks->where('student_id', $student->id);
+            }
+
+            $marks = $marks->paginate($perPage);
 
             return MarkResource::collection($marks);
+        } catch (HttpException $e) {
+            throw $e;
         } catch (Throwable $th) {
             Log::channel('err')->error(time() . " There was an error MarkController@marks " . $th->getMessage());
 
